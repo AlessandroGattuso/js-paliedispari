4c4d2249c4cbd197c18bd006d679d161b12703f3
Alessandro <alessandrogattuso@yahoo.it>
2022-12-05 15:28:16 +0100

    improve the result on the console

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -8,8 +8,14 @@ const word = prompt("Type a word");
 
 //Check if the word is a palindrome word with optimal solution
 (IsPalindrome_optimal(word)) ? 
-console.log("The word you chose is a palindrome word") :
-console.log("The word you chose is not a palindrome word");
+  console.log("The word you chose is a palindrome word") :
+  console.log("The word you chose is not a palindrome word");
+
+//Palindrome check function
+function IsPalindrome(word){
+  //Take the string, reverse it and compare it with our word
+  return (word.split("").reverse().join("") === word) ? true : false;
+}
 
 //Optimal Palindrome check function
 function IsPalindrome_optimal(word){ 
@@ -23,10 +29,4 @@ function IsPalindrome_optimal(word){
       --j;
   }
   return true;
-}
-
-//Palindrome check function
-function IsPalindrome(word){
-  //Take the string, reverse it and compare it with our word
-  return (word.split("").reverse().join("") == word) ? true : false;
 }
--- Pari_e_Dispari/js/script.js
+++ Pari_e_Dispari/js/script.js
@@ -10,18 +10,19 @@ do{
   userNumber = parseInt(prompt("Now chose a number between 1 and 5"));
 }while(userNumber < 1 && userNumber > 5);
 
+console.log("You chose " + chose.toLowerCase());
 //Generate a random number for the IA
 const IANumber = numberRandom(1,6);
 
 //Check if the sum of IAnumber and the userNumber is even and the user chose even, the user win
 if(IsEven(IANumber + userNumber) && chose.toLowerCase() == "even")
-    console.log("Hai vinto")
+    console.log(`${IANumber} + ${userNumber} = ${IANumber + userNumber}  You win`)
 //Check if the sum of IAnumber and the userNumber is odd and the user chose odd, the user win
 else if(!IsEven(IANumber + userNumber) && chose.toLowerCase() == "odd")
-    console.log("Hai vinto")
+    console.log(`${IANumber} + ${userNumber} = ${IANumber + userNumber}  You win`)
 //else the user lose
 else
-    console.log("hai perso")
+    console.log(`${IANumber} + ${userNumber} = ${IANumber + userNumber}  You lose`)
 
 //Random number function 
 function numberRandom(min,max){
