5380d39290b5f2bade5ad74b70aef903f88b7b97
Alessandro <alessandrogattuso@yahoo.it>
2022-12-05 13:54:06 +0100

    exersise done

---
+++ Pari_e_Dispari/js/script.js
@@ -0,0 +1,32 @@
+//Asking the user to chose between even or odd and validate it
+let chose;
+do{
+  chose = prompt("Chose even or odd");
+}while(chose.toLowerCase() != 'even' && chose.toLowerCase() != 'odd');
+
+//Asking the user to pick a number between 1 and 5 and validate it
+let userNumber;
+do{
+  userNumber = parseInt(prompt("Now chose a number between 1 and 5"));
+}while(userNumber < 1 && userNumber > 5);
+
+//Generate a random number for the IA
+const IANumber = numberRandom(1,6);
+console.log(IANumber);
+
+//Check if the sum of IAnumber and the userNumber is even and the user chose even, the user win
+if(IsEven(IANumber + userNumber) && chose.toLowerCase() == "even")
+    console.log("Hai vinto")
+//else the user lose
+else
+    console.log("hai perso")
+
+//Random number function 
+function numberRandom(min,max){
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+//Function to check of the number is even
+function IsEven(n){  
+  return (!(n%2)) ? true : false;
+}
